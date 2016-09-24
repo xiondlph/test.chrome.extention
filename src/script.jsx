0c8933acd8a1f91388760d0a81c381fef02648d1
Ismailov Shuhrat <admin@ismax.ru>
2016-09-24 22:59:51 +0300

    init chrome extention script

---
+++ src/script.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class SearchForm extends React.Component {
+    render() {
+        return <h1>Hello</h1>
+    }
+}
+
+ReactDOM.render(<SearchForm/>, document.body);
